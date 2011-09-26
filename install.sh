c54a88dc928ac15db17a640c95698aab8782e8ca
Dragon Jake <wjake@wjake.com>
2011-09-26 19:51:46 +0200

    fixed installation, script didn't copy files which didn't exist in home directory

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ DIR="$( dirname "$0" )"
 exec 5<"$DIR"/filelist.txt
 while read -u5 file; do
 	original="$file"
-	# we support only files
-	[ ! -f "$DST_DIR/$original" ] && continue
+	# skip empty lines
+	[ -z "$original" ] && continue
 	
 	# normalize the filename (remote dot)
 	[ "${file:0:1}" == "." ] && file="${file:1}"
