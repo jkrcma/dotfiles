91cd58c66c4283f937a5a14efca53d245ae0cda5
Jakub Krcma <jkrcma@gmail.com>
2019-09-18 17:10:04 +0200

    Show top bar also on every other monitor

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -9,5 +9,12 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar top -c ~/.config/polybar/config-top.ini &
+if type "xrandr"; then
+	for m in $( xrandr --query | grep " connected" | cut -d" " -f1 ); do
+		MONITOR=$m polybar top -c ~/.config/polybar/config-top.ini &
+	done
+else
+	polybar top -c ~/.config/polybar/config-top.ini &
+fi
+
 polybar bottom -c ~/.config/polybar/config-bottom.ini &
