31225e498dd114219ea63d0beea24fcac7aed31e
Jakub Krcma <jkrcma@gmail.com>
2020-06-23 10:06:46 +0200

    Update GohuFont name

--- config/dunst/dmenu.sh
+++ config/dunst/dmenu.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/dmenu -p dunst: -fn "Gohufont:size=8" -l 2 -nb '#112526' -nf '#ae9793' -sb '#ee7313'
+/usr/bin/dmenu -p dunst: -fn "Gohu GohuFont:size=8" -l 2 -nb '#112526' -nf '#ae9793' -sb '#ee7313'
