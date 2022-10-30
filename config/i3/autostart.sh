a266eca76fbd249c2a34c057d04bfa5cb5de863e
Jakub Krcma <jkrcma@gmail.com>
2022-10-31 00:52:17 +0100

    i3: Define workspace layouts and rework autostart

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
-i3-msg 'workspace 1; exec dex /usr/share/applications/google-chrome.desktop'
-sleep 2
-i3-msg 'workspace 2; exec dex /usr/share/applications/termite.desktop'
-i3-msg 'exec dex /usr/share/applications/termite.desktop'
-sleep 0.5
-i3-msg 'workspace 3; exec dex /usr/share/applications/visual-studio-code.desktop'
-sleep 2
-i3-msg 'workspace 10; exec dex /usr/share/applications/telegramdesktop.desktop'
-sleep 2
+layout_path=$HOME/.config/i3/layouts
+r_shared=/usr/share/applications
+r_local=$HOME/.local/share/applications
 
-i3-msg 'workspace 1'
+# Apply layouts
+for i in $( seq 1 10 ); do
+	[ -f $layout_path/w$i.json ] && i3-msg "workspace $i; append_layout $layout_path/w$i.json"
+done
+
+# Most are Electron / Chrome apps, they start slooow
+i3-msg "exec dex $r_shared/slack.desktop; exec dex $r_local/google-chrome-proton.desktop"
+sleep 5
+i3-msg "exec dex $r_shared/Alacritty.desktop"
+i3-msg "exec dex $r_shared/Alacritty.desktop"
+sleep 1
+i3-msg "exec dex $r_shared/visual-studio-code.desktop"
+sleep 5
+i3-msg "exec dex $r_shared/obsidian.desktop"
+sleep 2
+i3-msg "exec dex $r_shared/google-chrome.desktop; exec dex $r_local/google-chrome-orchideio.desktop"
+sleep 5
+i3-msg "exec dex $r_shared/bitwarden.desktop"
+sleep 2
+i3-msg "exec dex $r_shared/telegramdesktop.desktop; exec dex $r_shared/discord.desktop"
+sleep 5
+
+i3-msg "workspace 1"
